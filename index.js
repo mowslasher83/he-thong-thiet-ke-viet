1d7161d9265cd9cf9ff783e237889dc6690aa197
mowslasher83 <radeswaled@hotmail.com>
2025-09-03 12:22:25 -0500

    Reordered components for better readability and maintainability.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ module.exports = {
   // UI components
   Button: require('./components/Button'),
   Checkbox: require('./components/Checkbox'),
+  Dropdown: require('./components/Dropdown'),
   Input: require('./components/Input'),
+  Modal: require('./components/Modal'),
   Textarea: require('./components/Textarea'),
   Tooltip: require('./components/Tooltip'),
-  Dropdown: require('./components/Dropdown'),
-  Modal: require('./components/Modal'),
   // Additional components can be added here
 };
