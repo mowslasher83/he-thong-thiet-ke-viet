afa3b01119e20882ea8de04bf8231aeb648d8c7d
mowslasher83 <radeswaled@hotmail.com>
2025-09-03 12:18:31 -0500

    Update index.js to add Textarea component

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   // Thêm các thành phần UI ở đây
   Button: require('./components/Button'),
   Input: require('./components/Input'),
+  Textarea: require('./components/Textarea'),
   // ...
 };
